1644bb0bc2520815fe8158ae031fe5b6a0834953
ALEJANDRO <alejandro@MacBook-Air-de-ALEJANDRO.local>
2025-07-10 19:55:52 -0500

    firs

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import Controllers.Graph;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        // Create a new graph
+        Graph graph = new Graph();
+        // Add nodes to the graph
+        graph.addNodo(0);
+        graph.addNodo(5);
+        graph.addNodo(3);
+        graph.addNodo(2);
+        graph.addNodo(1);
+        graph.addNodo(9);
+        graph.addNodo(4);
+        graph.addNodo(8);
+        graph.addNodo(7);
+        // Add edges between nodes
+       
+        // Print the graph
+    }
+}
